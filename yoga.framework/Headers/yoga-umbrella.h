c351716aaa4117a23d9fe7f183a6fb8170643f55
Floris Snuif <florissnuif@Floriss-MBP.home>
2019-04-10 20:40:04 +0200

    0.3.0

--- yoga.framework/Headers/yoga-umbrella.h
+++ yoga.framework/Headers/yoga-umbrella.h
@@ -12,8 +12,7 @@
 
 #import "YGEnums.h"
 #import "YGMacros.h"
-#import "YGNodeList.h"
-#import "Yoga-internal.h"
+#import "YGValue.h"
 #import "Yoga.h"
 
 FOUNDATION_EXPORT double yogaVersionNumber;
